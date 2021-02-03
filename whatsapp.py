cf553c1ca1e394d7833f17786f3db9efa2a90561
luckyIITR <luckykushwaha56@gmail.com>
2021-02-03 22:54:28 +0530

    file added

---
+++ whatsapp.py
@@ -0,0 +1,26 @@
+# importing the module 
+import pywhatkit
+
+
+pywhatkit.search("Python")
+
+
+
+# # using Exception Handling to avoid
+# # unprecedented errors
+# try:
+#
+
+
+#     # sending message to reciever
+#     # using pywhatkit
+#     pywhatkit.sendwhatmsg("+917424961628",
+#                           "Hello This message is from Python automate test!!",
+#                           22, 33)
+#     print("Successfully Sent!")
+#
+# except:
+#
+#     # handling exception
+#     # and printing error message
+#     print("An Unexpected Error!")
